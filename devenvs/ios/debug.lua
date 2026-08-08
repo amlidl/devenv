6aad530680afef24f59d8d6fff41e65158dcc58a
Andrés Martínez <Andres.Martinez_Garcia@digits.schwarz>
2026-08-08 13:31:46 +0200

    ios debug

--- devenvs/ios/debug.lua
+++ devenvs/ios/debug.lua
@@ -1,51 +1,24 @@
 return {
     setup_dap = function(dap)
-        dap.adapters.lldb = {
+        dap.adapters.xcede = {
             type = "executable",
-            command = "xcrun",
-            args = { "lldb-dap" },
-            name = "apple-lldb",
+            command = "xcede",
+            name = "xcede-debug",
             options = {
                 initialize_timeout_sec = 9999,
             }
         }
         dap.configurations.swift = {
             {
-                name = "Attach (iOS Simulator)",
-                type = "lldb",
+                name = "Attach",
+                type = "xcede",
                 request = "attach",
-                pid = function() 
-                    -- TODO Improve and pick the pid based on path
-                    local filter = vim.fn.input('Filter process name: ')
-                    return require('dap.utils').pick_process({ filter = filter })
-                end,
-                cwd = vim.fn.getcwd(),
             },
             {
-                name = "Wait for launch (iOS Simulator)",
-                type = "lldb",
-                request = "attach",
-                program = function()
-                    path = vim.fn.system("ios exec-path")
-                    path = vim.trim(path)
-                    return path
-                end,
-                waitFor = true,
-                stopOnEntry = true
-            },
-            {
-                name = "Attach (iPhone)",
-                type = "lldb",
-                request = "attach",
-                cwd = vim.fn.getcwd(),
-                initCommands = function()
-                    iphone_name = vim.fn.system("ios iphone-name")
-                    iphone_name = vim.trim(iphone_name)
-
-                    -- TODO
-                    return {
-                    }
-                end
+                name = "Launch",
+                type = "xcede",
+                request = "launch",
+                program = "xcede:"
             }
         }
     end
